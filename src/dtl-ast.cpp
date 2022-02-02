ce6ee9a1ff3be98a4b86e8d84fcecf185a707ee3
Ben Mather <bwhmather@bwhmather.com>
2022-02-02 00:15:53 +0000

    Initial commit

---
+++ src/dtl-ast.cpp
@@ -0,0 +1,189 @@
+#include "dtl-ast.hpp"
+
+#include <stddef.h>
+#include <vector>
+#include <string>
+#include <memory>
+
+namespace dtl {
+namespace ast {
+
+/* === Literals ============================================================= */
+
+void String::accept(NodeVisitor& visitor) {
+    visitor.visit_string(*this);
+}
+
+/* === Columns ============================================================== */
+
+void ColumnName::accept(NodeVisitor& visitor) {
+    this->accept(static_cast<ColumnNameVisitor&>(visitor));
+}
+
+void UnqualifiedColumnName::accept(ColumnNameVisitor& visitor) {
+    visitor.visit_unqualified_column_name(*this);
+}
+
+void QualifiedColumnName::accept(ColumnNameVisitor& visitor) {
+    visitor.visit_qualified_column_name(*this);
+}
+
+void Expression::accept(NodeVisitor& visitor) {
+    this->accept(static_cast<ExpressionVisitor&>(visitor));
+}
+
+void ColumnReferenceExpression::accept(ExpressionVisitor& visitor) {
+    visitor.visit_column_reference_expression(*this);
+}
+
+void FunctionCallExpression::accept(ExpressionVisitor& visitor) {
+    visitor.visit_function_call_expression(*this);
+}
+
+void AddExpression::accept(ExpressionVisitor& visitor) {
+    visitor.visit_add_expression(*this);
+}
+
+void SubtractExpression::accept(ExpressionVisitor& visitor) {
+    visitor.visit_subtract_expression(*this);
+}
+
+void MultiplyExpression::accept(ExpressionVisitor& visitor) {
+    visitor.visit_multiply_expression(*this);
+}
+
+void DivideExpression::accept(ExpressionVisitor& visitor) {
+    visitor.visit_divide_expression(*this);
+}
+
+/* === Tables =============================================================== */
+
+void TableName::accept(NodeVisitor& visitor) {
+    visitor.visit_table_name(*this);
+}
+
+/* === Distinct ============================================================= */
+
+void DistinctClause::accept(NodeVisitor& visitor) {
+    visitor.visit_distinct_clause(*this);
+}
+
+/* === Column Bindings ====================================================== */
+
+void ColumnBinding::accept(NodeVisitor& visitor) {
+    this->accept(static_cast<ColumnBindingVisitor&>(visitor));
+}
+
+void WildcardColumnBinding::accept(ColumnBindingVisitor& visitor) {
+    visitor.visit_wildcard_column_binding(*this);
+}
+
+void ImplicitColumnBinding::accept(ColumnBindingVisitor& visitor) {
+    visitor.visit_implicit_column_binding(*this);
+}
+
+void AliasedColumnBinding::accept(ColumnBindingVisitor& visitor) {
+    visitor.visit_aliased_column_binding(*this);
+}
+
+/* === From ================================================================= */
+
+void TableBinding::accept(NodeVisitor& visitor) {
+    this->accept(static_cast<TableBindingVisitor&>(visitor));
+}
+
+void ImplicitTableBinding::accept(TableBindingVisitor& visitor) {
+    visitor.visit_implicit_table_binding(*this);
+}
+
+void AliasedTableBinding::accept(TableBindingVisitor& visitor) {
+    visitor.visit_aliased_table_binding(*this);
+}
+
+void FromClause::accept(NodeVisitor& visitor) {
+    visitor.visit_from_clause(*this);
+}
+
+/* === Joins ================================================================ */
+
+void JoinConstraint::accept(NodeVisitor& visitor) {
+    this->accept(static_cast<JoinConstraintVisitor&>(visitor));
+}
+
+void JoinOnConstraint::accept(JoinConstraintVisitor& visitor) {
+    visitor.visit_join_on_constraint(*this);
+}
+
+void JoinUsingConstraint::accept(JoinConstraintVisitor& visitor) {
+    visitor.visit_join_using_constraint(*this);
+}
+
+void JoinClause::accept(NodeVisitor& visitor) {
+    visitor.visit_join_clause(*this);
+}
+
+/* === Filtering ============================================================ */
+
+void WhereClause::accept(NodeVisitor& visitor) {
+    visitor.visit_where_clause(*this);
+}
+
+/* === Grouping ============================================================= */
+
+void GroupByClause::accept(NodeVisitor& visitor) {
+    visitor.visit_group_by_clause(*this);
+}
+
+/* === Table Expressions ==================================================== */
+
+void TableExpression::accept(NodeVisitor& visitor) {
+    this->accept(static_cast<TableExpressionVisitor&>(visitor));
+}
+
+void SelectExpression::accept(TableExpressionVisitor& visitor) {
+    visitor.visit_select_expression(*this);
+}
+
+void ImportExpression::accept(TableExpressionVisitor& visitor) {
+    visitor.visit_import_expression(*this);
+}
+
+void TableReferenceExpression::accept(TableExpressionVisitor& visitor) {
+    visitor.visit_table_reference_expression(*this);
+}
+
+/* === Statements =========================================================== */
+
+void Statement::accept(NodeVisitor& visitor) {
+    this->accept(static_cast<StatementVisitor&>(visitor));
+}
+
+void AssignmentStatement::accept(StatementVisitor& visitor) {
+    visitor.visit_assignment_statement(*this);
+}
+
+void UpdateStatement::accept(StatementVisitor& visitor) {
+    visitor.visit_update_statement(*this);
+}
+
+void DeleteStatement::accept(StatementVisitor& visitor) {
+    visitor.visit_delete_statement(*this);
+}
+
+void InsertStatement::accept(StatementVisitor& visitor) {
+    visitor.visit_insert_statement(*this);
+}
+
+void ExportStatement::accept(StatementVisitor& visitor) {
+    visitor.visit_export_statement(*this);
+}
+
+/* === Scripts ============================================================== */
+
+void Script::accept(NodeVisitor& visitor) {
+    visitor.visit_script(*this);
+}
+
+} /* namespace ast */
+} /* namespace dtl */
+
---
+++ src/dtl-ast.hpp
@@ -0,0 +1,422 @@
+#pragma once
+
+#include <stddef.h>
+#include <vector>
+#include <string>
+#include <memory>
+
+namespace dtl {
+namespace ast {
+
+class ColumnNameVisitor;
+class ExpressionVisitor;
+class ColumnBindingVisitor;
+class TableBindingVisitor;
+class JoinConstraintVisitor;
+class TableExpressionVisitor;
+class StatementVisitor;
+class NodeVisitor;
+
+class TableExpression;
+
+
+class Location {
+  public:
+    size_t offset;
+    size_t lineno;
+    size_t column;
+};
+
+class Node {
+  public:
+    Location start;
+    Location end;
+
+    virtual ~Node() {}
+    virtual void accept(NodeVisitor& visitor) = 0;
+};
+
+/* === Literals ============================================================= */
+
+class String final : public Node {
+  public:
+    std::string value;
+
+    void accept(NodeVisitor& visitor) override final;
+};
+
+/* === Columns ============================================================== */
+
+class ColumnName : public Node {
+  public:
+    void accept(NodeVisitor& visitor) override final;
+    virtual void accept(ColumnNameVisitor& visitor) = 0;
+};
+
+class UnqualifiedColumnName final : public ColumnName {
+  public:
+    std::string column_name;
+
+    void accept(ColumnNameVisitor& visitor) override final;
+};
+
+class QualifiedColumnName final : public ColumnName {
+  public:
+    std::string table_name;
+    std::string column_name;
+
+    void accept(ColumnNameVisitor& visitor) override final;
+};
+
+class Expression : public Node {
+  public:
+    void accept(NodeVisitor& visitor) override final;
+    virtual void accept(ExpressionVisitor& visitor) = 0;
+};
+
+class ColumnReferenceExpression final : public Expression {
+  public:
+    std::unique_ptr<ColumnName> name;
+
+    void accept(ExpressionVisitor& visitor) override final;
+};
+
+class FunctionCallExpression final : public Expression {
+  public:
+    std::string name;
+    std::vector<std::unique_ptr<Expression>> arguments;
+
+    void accept(ExpressionVisitor& visitor) override final;
+};
+
+class AddExpression final : public Expression {
+  public:
+    std::unique_ptr<Expression> left;
+    std::unique_ptr<Expression> right;
+
+    void accept(ExpressionVisitor& visitor) override final;
+};
+
+class SubtractExpression final : public Expression {
+  public:
+    std::unique_ptr<Expression> left;
+    std::unique_ptr<Expression> right;
+
+    void accept(ExpressionVisitor& visitor) override final;
+};
+
+class MultiplyExpression final : public Expression {
+  public:
+    std::unique_ptr<Expression> left;
+    std::unique_ptr<Expression> right;
+
+    void accept(ExpressionVisitor& visitor) override final;
+};
+
+class DivideExpression final : public Expression {
+  public:
+    std::unique_ptr<Expression> left;
+    std::unique_ptr<Expression> right;
+
+    void accept(ExpressionVisitor& visitor) override final;
+};
+
+/* === Tables =============================================================== */
+
+class TableName final : public Node {
+  public:
+    std::string table_name;
+
+    void accept(NodeVisitor& visitor) override final;
+};
+
+/* === Distinct ============================================================= */
+
+class DistinctClause final : public Node {
+  public:
+    bool consecutive;
+
+    void accept(NodeVisitor& visitor) override final;
+};
+
+/* === Column Bindings ====================================================== */
+
+class ColumnBinding : public Node {
+  public:
+    void accept(NodeVisitor& visitor) override final;
+    virtual void accept(ColumnBindingVisitor& visitor) = 0;
+};
+
+class WildcardColumnBinding final : public ColumnBinding {
+  public:
+    void accept(ColumnBindingVisitor& visitor) override final;
+};
+
+class ImplicitColumnBinding final : public ColumnBinding {
+  public:
+    std::unique_ptr<Expression> expression;
+
+    void accept(ColumnBindingVisitor& visitor) override final;
+};
+
+class AliasedColumnBinding final : public ColumnBinding {
+  public:
+    std::unique_ptr<Expression> expression;
+    std::string alias;
+
+    void accept(ColumnBindingVisitor& visitor) override final;
+};
+
+/* === From ================================================================= */
+
+class TableBinding : public Node {
+  public:
+    void accept(NodeVisitor& visitor) override final;
+    virtual void accept(TableBindingVisitor& visitor) = 0;
+};
+
+class ImplicitTableBinding final : public TableBinding {
+  public:
+    std::unique_ptr<TableExpression> expression;
+
+    void accept(TableBindingVisitor& visitor) override final;
+};
+
+class AliasedTableBinding final : public TableBinding {
+  public:
+    std::unique_ptr<TableExpression> expression;
+    std::string alias;
+
+    void accept(TableBindingVisitor& visitor) override final;
+};
+
+class FromClause final : public Node {
+  public:
+    void accept(NodeVisitor& visitor) override final;
+};
+
+/* === Joins ================================================================ */
+
+class JoinConstraint : public Node {
+  public:
+    void accept(NodeVisitor& visitor) override final;
+    virtual void accept(JoinConstraintVisitor& visitor) = 0;
+};
+
+class JoinOnConstraint final : public JoinConstraint {
+  public:
+    std::unique_ptr<Expression> predicate;
+
+    void accept(JoinConstraintVisitor& visitor) override final;
+};
+
+class JoinUsingConstraint final : public JoinConstraint {
+  public:
+    std::vector<std::unique_ptr<UnqualifiedColumnName>> columns;
+
+    void accept(JoinConstraintVisitor& visitor) override final;
+};
+
+class JoinClause final : public Node {
+  public:
+    std::unique_ptr<TableBinding> table;
+    std::unique_ptr<JoinConstraint> constraint;
+
+    void accept(NodeVisitor& visitor) override final;
+};
+
+/* === Filtering ============================================================ */
+
+class WhereClause final : public Node {
+  public:
+    std::unique_ptr<Expression> predicate;
+
+    void accept(NodeVisitor& visitor) override final;
+};
+
+/* === Grouping ============================================================= */
+
+class GroupByClause final : public Node {
+  public:
+    bool consecutive;
+    std::vector<std::unique_ptr<Expression>> pattern;
+
+    void accept(NodeVisitor& visitor) override final;
+};
+
+/* === Table Expressions ==================================================== */
+
+class TableExpression : public Node {
+  public:
+    void accept(NodeVisitor& visitor) override final;
+    virtual void accept(TableExpressionVisitor& visitor) = 0;
+};
+
+class SelectExpression final : public TableExpression {
+  public:
+    std::unique_ptr<DistinctClause> distinct;  /* nullable */
+    std::vector<ColumnBinding> columns;
+    std::unique_ptr<FromClause> source;
+    std::vector<JoinClause> joins;
+    std::unique_ptr<WhereClause> where;  /* nullable */
+    std::unique_ptr<GroupByClause> group_by;  /* nullable */
+
+    void accept(TableExpressionVisitor& visitor) override final;
+};
+
+class ImportExpression final : public TableExpression {
+  public:
+    std::unique_ptr<String> location;
+
+    void accept(TableExpressionVisitor& visitor) override final;
+};
+
+class TableReferenceExpression final : public TableExpression {
+  public:
+    std::string name;
+
+    void accept(TableExpressionVisitor& visitor) override final;
+};
+
+/* === Statements =========================================================== */
+
+class Statement : public Node {
+  public:
+    void accept(NodeVisitor& visitor) override final;
+    virtual void accept(StatementVisitor& visitor) = 0;
+};
+
+class AssignmentStatement final : public Statement {
+  public:
+    std::unique_ptr<TableName> target;
+    std::unique_ptr<TableExpression> expression;
+
+    void accept(StatementVisitor& visitor) override final;
+};
+
+class UpdateStatement final : public Statement {
+  public:
+    /* TODO */
+    void accept(StatementVisitor& visitor) override final;
+};
+
+class DeleteStatement final : public Statement {
+  public:
+    /* TODO */
+    void accept(StatementVisitor& visitor) override final;
+};
+
+class InsertStatement final : public Statement {
+  public:
+    /* TODO */
+    void accept(StatementVisitor& visitor) override final;
+};
+
+class ExportStatement final : public Statement {
+  public:
+    std::unique_ptr<String> location;
+    std::unique_ptr<TableExpression> expression;
+
+    void accept(StatementVisitor& visitor) override final;
+};
+
+/* === Scripts ============================================================== */
+
+class Script final : public Node {
+  public:
+    std::vector<std::unique_ptr<Statement>> statements;
+
+    void accept(NodeVisitor& visitor) override final;
+};
+
+/* === Visitors ============================================================= */
+
+
+class ColumnNameVisitor {
+  public:
+    virtual ~ColumnNameVisitor() {}
+
+    virtual void visit_unqualified_column_name(UnqualifiedColumnName& column_name) = 0;
+    virtual void visit_qualified_column_name(QualifiedColumnName& column_name) = 0;
+};
+
+class ExpressionVisitor {
+  public:
+    virtual ~ExpressionVisitor() {}
+
+    virtual void visit_column_reference_expression(ColumnReferenceExpression& expr) = 0;
+    virtual void visit_function_call_expression(FunctionCallExpression& expr) = 0;
+    virtual void visit_add_expression(AddExpression& expr) = 0;
+    virtual void visit_subtract_expression(SubtractExpression& expr) = 0;
+    virtual void visit_multiply_expression(MultiplyExpression& expr) = 0;
+    virtual void visit_divide_expression(DivideExpression& expr) = 0;
+};
+
+class ColumnBindingVisitor {
+  public:
+    virtual ~ColumnBindingVisitor() {}
+
+    virtual void visit_wildcard_column_binding(WildcardColumnBinding& binding) = 0;
+    virtual void visit_implicit_column_binding(ImplicitColumnBinding& binding) = 0;
+    virtual void visit_aliased_column_binding(AliasedColumnBinding& binding) = 0;
+};
+
+class TableBindingVisitor {
+  public:
+    virtual ~TableBindingVisitor() {}
+
+    virtual void visit_implicit_table_binding(ImplicitTableBinding& binding) = 0;
+    virtual void visit_aliased_table_binding(AliasedTableBinding& binding) = 0;
+};
+
+class JoinConstraintVisitor {
+  public:
+    virtual ~JoinConstraintVisitor() {}
+
+    virtual void visit_join_on_constraint(JoinOnConstraint& constraint) = 0;
+    virtual void visit_join_using_constraint(JoinUsingConstraint& constraint) = 0;
+};
+
+class TableExpressionVisitor {
+  public:
+    virtual ~TableExpressionVisitor() {}
+
+    virtual void visit_select_expression(SelectExpression& expr) = 0;
+    virtual void visit_import_expression(ImportExpression& expr) = 0;
+    virtual void visit_table_reference_expression(TableReferenceExpression& expr) = 0;
+};
+
+class StatementVisitor {
+  public:
+    virtual ~StatementVisitor() {}
+
+    virtual void visit_assignment_statement(AssignmentStatement& visitor) = 0;
+    virtual void visit_update_statement(UpdateStatement& visitor) = 0;
+    virtual void visit_delete_statement(DeleteStatement& visitor) = 0;
+    virtual void visit_insert_statement(InsertStatement& visitor) = 0;
+    virtual void visit_export_statement(ExportStatement& visitor) = 0;
+};
+
+class NodeVisitor :
+    public ColumnNameVisitor,
+    public ExpressionVisitor,
+    public ColumnBindingVisitor,
+    public TableBindingVisitor,
+    public JoinConstraintVisitor,
+    public TableExpressionVisitor,
+    public StatementVisitor
+{
+  public:
+    virtual void visit_string(String& string) = 0;
+    virtual void visit_table_name(TableName& table_name) = 0;
+    virtual void visit_distinct_clause(DistinctClause& clause) = 0;
+    virtual void visit_from_clause(FromClause& clause) = 0;
+    virtual void visit_join_clause(JoinClause& clause) = 0;
+    virtual void visit_where_clause(WhereClause& clause) = 0;
+    virtual void visit_group_by_clause(GroupByClause& clause) = 0;
+    virtual void visit_script(Script& script) = 0;
+};
+
+} /* namespace ast */
+} /* namespace dtl */
+
