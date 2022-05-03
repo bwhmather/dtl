e365bf8cec07e04aa78e40ba06b4bf6cd2de0297
Ben Mather <bwhmather@bwhmather.com>
2022-05-03 22:16:18 +0100

    Get main.cpp building

--- src/main.cpp
+++ src/main.cpp
@@ -1,30 +1,16 @@
+#include <filesystem>
+#include <fstream>
+#include <iostream>
+#include <iterator>
+#include <string>
+
+#include "dtl-ast.hpp"
+#include "dtl-ast-to-ir.hpp"
+#include "dtl-eval.hpp"
+#include "dtl-io-filesystem.hpp"
+#include "dtl-ir.hpp"
 #include "dtl-parser.hpp"
-
-
-static std::shared_ptr<arrow::Table> load_input_file(std::string& path) {
-    std::shard_ptr<arrow::io::ReadableFile> input_file;
-    PARQUET_ASSIGN_OR_THROW(
-        input_file,
-        arrow::io::ReadableFile::Open(input_path, arrow::default_memory_pool())
-    );
-
-    std::unique_ptr<parquet::arrow::FileReader> reader;
-    PARQUET_THROW_NOT_OK
-        parquet::arrow::OpenFile(
-            input_file, arrow::default_memory_pool(), &reader
-        )
-    );
-
-    std::shared_ptr<arrow::Table> table;
-    PARQUET_THROW_NOT_OK(reader->ReadTable(&table));
-    return table;
-}
-
-
-static std::unordered_map<std::string, std::shared_ptr<arrow::Table>> load_input_dir(std::string) {
-
-}
-
+#include "dtl-tokenizer.hpp"
 
 int main(int argc, char *argv[]) {
     assert(argc == 4);
@@ -33,15 +19,15 @@ int main(int argc, char *argv[]) {
     std::filesystem::path input_dir(argv[2]);
     std::filesystem::path output_dir(argv[3]);
 
+    std::ifstream script_file(script_path);
+    std::string source(std::istreambuf_iterator<char>(script_file), {});
 
-
-
-    std::string source = (
-        "input = IMPORT 'input';\n"
-        "output = SELECT old_column AS new_column FROM input;\n"
-        "EXPORT output TO 'output';\n"
-    );
+    auto importer = dtl::io::filesystem_importer(input_dir);
+    auto exporter = dtl::io::filesystem_exporter(output_dir);
 
     dtl::tokenizer::Tokenizer tokenizer(source.begin(), source.end());
-    dtl::parser::parse(tokenizer);
+    auto ast = dtl::parser::parse(tokenizer);
+    auto program = dtl::ast::to_ir(*ast, *importer);
+
+    dtl::eval::eval(program, *importer, *exporter);
 }
