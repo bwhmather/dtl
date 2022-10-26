e8275050e7dbf7455b46558341dbfb333cec90c5
Ben Mather <bwhmather@bwhmather.com>
2022-10-26 14:23:41 +0100

    Start of trivial C++ wrapper for libuuid

---
+++ src/dtl-uuid.cpp
@@ -0,0 +1,17 @@
+#include "dtl-uuid.hpp"
+
+#include <array>
+#include <memory>
+#include <uuid/uuid.h>
+
+namespace dtl {
+
+UUID uuid4(void) {
+    uuid_t data;
+    uuid_generate_random(data);
+    return UUID(reinterpret_cast<std::array<std::byte, 16>&>(data));
+}
+
+
+} /* namespace dtl */
+
---
+++ src/dtl-uuid.hpp
@@ -0,0 +1,21 @@
+#pragma once
+
+#include <array>
+#include <cstddef>
+#include <memory>
+#include <string>
+
+namespace dtl {
+
+class UUID {
+    std::array<std::byte, 16> data;
+    friend bool operator == (UUID const & lhs, UUID const & rhs) noexcept;
+  public:
+    constexpr UUID(
+        std::array<std::byte, 16> const& source
+    ) noexcept : data{source} {};
+};
+
+UUID uuid4(void);
+
+} /* namespace dtl */
