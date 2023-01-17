de4a255f8db8e7e6cd9bec9450d25645037bf756
Ben Mather <bwhmather@bwhmather.com>
2023-01-17 15:08:32 +0000

    Add a few more tests for mapping table columns using select

---
+++ tests/end-to-end/test_duplicate_columns.py
@@ -0,0 +1,20 @@
+import pyarrow as pa
+
+import dtl
+
+
+def main():
+    src = """
+    WITH input AS IMPORT 'input';
+    WITH output AS SELECT a, a AS b FROM input;
+    EXPORT output TO 'output';
+    """
+    inputs = {
+        "input": pa.table({"a": [1, 2, 3, 4], "b": [5, 6, 7, 8], "c": [9, 10, 11, 12]})
+    }
+    outputs, trace = dtl.run(src, inputs=inputs)
+    assert outputs["output"] == pa.table({"a": [1, 2, 3, 4], "b": [1, 2, 3, 4]})
+
+
+if __name__ == "__main__":
+    main()
