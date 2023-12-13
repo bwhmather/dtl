73c0f06488eab20cda00ce15ed15a0c35268823c
Ben Mather <bwhmather@bwhmather.com>
2023-12-13 21:01:53 +0000

    Remove dtl::ast::Type

--- src/dtl-ast.hpp
+++ src/dtl-ast.hpp
@@ -13,86 +13,10 @@
 namespace dtl {
 namespace ast {
 
-enum class Type {
-    // Literals.
-    LITERAL = 0x0100,
-    STRING = 1,
-
-    // Columns.
-    COLUMN_NAME = 0x0200,
-    UNQUALIFIED_COLUMN_NAME,
-    QUALIFIED_COLUMN_NAME,
-
-    // Expressions.
-    EXPRESSION = 0x0300,
-    COLUMN_REFERENCE_EXPRESSION,
-    LITERAL_EXPRESSION,
-    FUNCTION_CALL_EXPRESSION,
-    EQUAL_TO_EXPRESSION,
-    LESS_THAN_EXPRESSION,
-    LESS_THAN_EQUAL_EXPRESSION,
-    GREATER_THAN_EXPRESSION,
-    GREATER_THAN_EQUAL_EXPRESSION,
-    ADD_EXPRESSION,
-    SUBTRACT_EXPRESSION,
-    MULTIPLY_EXPRESSION,
-    DIVIDE_EXPRESSION,
-
-    // Tables.
-    TABLE_NAME = 0x0400,
-
-    // Distinct.
-    DISTINCT_CLAUSE = 0x0500,
-
-    // Column Bindings.
-    COLUMN_BINDING = 0x0600,
-    WILDCARD_COLUMN_BINDING,
-    IMPLICIT_COLUMN_BINDING,
-    ALIASED_COLUMN_BINDING,
-
-    // From.
-    TABLE_BINDING = 0x0700,
-    IMPLICIT_TABLE_BINDING,
-    ALIASED_TABLE_BINDING,
-    FROM_CLAUSE,
-
-    // Joins.
-    JOIN_CONSTRAINT = 0x0800,
-    JOIN_ON_CONSTRAINT,
-    JOIN_USING_CONSTRAINT,
-    JOIN_CLAUSE = 0x0900,
-
-    // Filtering.
-    WHERE_CLAUSE = 0x0A00,
-
-    // Grouping.
-    GROUP_BY_CLAUSE = 0x0B00,
-    // Table Expressions.
-    TABLE_EXPRESSION = 0x0C00,
-    SELECT_EXPRESSION,
-    IMPORT_EXPRESSION,
-    TABLE_REFERENCE_EXPRESSION,
-
-    // Statements.
-    STATEMENT = 0x0D00,
-    ASSIGNMENT_STATEMENT,
-    UPDATE_STATEMENT,
-    DELETE_STATEMENT,
-    INSERT_STATEMENT,
-    EXPORT_STATEMENT,
-    BEGIN_STATEMENT,
-
-    // Scripts.
-    SCRIPT = 0x0E000,
-};
-
 class Node {
   public:
     Location start;
     Location end;
-
-    virtual Type
-    type() const = 0;
 
     virtual ~Node() {}
 };
@@ -185,9 +109,6 @@ class Script;
 
 class String final : public Node {
   public:
-    Type
-    type() const override final;
-
     std::string value;
 };
 
@@ -195,17 +116,11 @@ class String final : public Node {
 
 class UnqualifiedColumnName final : public Node {
   public:
-    Type
-    type() const override final;
-
     std::string column_name;
 };
 
 class QualifiedColumnName final : public Node {
   public:
-    Type
-    type() const override final;
-
     std::string table_name;
     std::string column_name;
 };
@@ -214,106 +129,70 @@ class QualifiedColumnName final : public Node {
 
 class ColumnReferenceExpression final : public Node {
   public:
-    Type
-    type() const override final;
-
     dtl::unique_variant_ptr_t<const ColumnName> name;
 };
 
 class LiteralExpression final : public Node {
   public:
-    Type
-    type() const override final;
-
     dtl::unique_variant_ptr_t<const Literal> value;
 };
 
 class FunctionCallExpression final : public Node {
   public:
-    Type
-    type() const override final;
-
     std::string name;
     std::vector<dtl::unique_variant_ptr_t<const Expression>> arguments;
 };
 
 class EqualToExpression final : public Node {
   public:
-    Type
-    type() const override final;
-
     dtl::unique_variant_ptr_t<const Expression> left;
     dtl::unique_variant_ptr_t<const Expression> right;
 };
 
 class LessThanExpression final : public Node {
   public:
-    Type
-    type() const override final;
-
     dtl::unique_variant_ptr_t<const Expression> left;
     dtl::unique_variant_ptr_t<const Expression> right;
 };
 
 class LessThanEqualExpression final : public Node {
   public:
-    Type
-    type() const override final;
-
     dtl::unique_variant_ptr_t<const Expression> left;
     dtl::unique_variant_ptr_t<const Expression> right;
 };
 
 class GreaterThanExpression final : public Node {
   public:
-    Type
-    type() const override final;
-
     dtl::unique_variant_ptr_t<const Expression> left;
     dtl::unique_variant_ptr_t<const Expression> right;
 };
 
 class GreaterThanEqualExpression final : public Node {
   public:
-    Type
-    type() const override final;
-
     dtl::unique_variant_ptr_t<const Expression> left;
     dtl::unique_variant_ptr_t<const Expression> right;
 };
 
 class AddExpression final : public Node {
   public:
-    Type
-    type() const override final;
-
     dtl::unique_variant_ptr_t<const Expression> left;
     dtl::unique_variant_ptr_t<const Expression> right;
 };
 
 class SubtractExpression final : public Node {
   public:
-    Type
-    type() const override final;
-
     dtl::unique_variant_ptr_t<const Expression> left;
     dtl::unique_variant_ptr_t<const Expression> right;
 };
 
 class MultiplyExpression final : public Node {
   public:
-    Type
-    type() const override final;
-
     dtl::unique_variant_ptr_t<const Expression> left;
     dtl::unique_variant_ptr_t<const Expression> right;
 };
 
 class DivideExpression final : public Node {
   public:
-    Type
-    type() const override final;
-
     dtl::unique_variant_ptr_t<const Expression> left;
     dtl::unique_variant_ptr_t<const Expression> right;
 };
@@ -322,9 +201,6 @@ class DivideExpression final : public Node {
 
 class TableName final : public Node {
   public:
-    Type
-    type() const override final;
-
     std::string table_name;
 };
 
@@ -332,9 +208,6 @@ class TableName final : public Node {
 
 class DistinctClause final : public Node {
   public:
-    Type
-    type() const override final;
-
     bool consecutive;
 };
 
@@ -342,23 +215,15 @@ class DistinctClause final : public Node {
 
 class WildcardColumnBinding final : public Node {
   public:
-    Type
-    type() const override final;
 };
 
 class ImplicitColumnBinding final : public Node {
   public:
-    Type
-    type() const override final;
-
     dtl::unique_variant_ptr_t<const Expression> expression;
 };
 
 class AliasedColumnBinding final : public Node {
   public:
-    Type
-    type() const override final;
-
     dtl::unique_variant_ptr_t<const Expression> expression;
     std::string alias;
 };
@@ -367,26 +232,17 @@ class AliasedColumnBinding final : public Node {
 
 class ImplicitTableBinding final : public Node {
   public:
-    Type
-    type() const override final;
-
     dtl::unique_variant_ptr_t<const TableExpression> expression;
 };
 
 class AliasedTableBinding final : public Node {
   public:
-    Type
-    type() const override final;
-
     dtl::unique_variant_ptr_t<const TableExpression> expression;
     std::string alias;
 };
 
 class FromClause final : public Node {
   public:
-    Type
-    type() const override final;
-
     dtl::unique_variant_ptr_t<const TableBinding> binding;
 };
 
@@ -394,25 +250,16 @@ class FromClause final : public Node {
 
 class JoinOnConstraint final : public Node {
   public:
-    Type
-    type() const override final;
-
     dtl::unique_variant_ptr_t<const Expression> predicate;
 };
 
 class JoinUsingConstraint final : public Node {
   public:
-    Type
-    type() const override final;
-
     std::vector<std::unique_ptr<const UnqualifiedColumnName>> columns;
 };
 
 class JoinClause final : public Node {
   public:
-    Type
-    type() const override final;
-
     dtl::unique_variant_ptr_t<const TableBinding> binding;
     dtl::unique_variant_ptr_t<const JoinConstraint> constraint;
 };
@@ -421,9 +268,6 @@ class JoinClause final : public Node {
 
 class WhereClause final : public Node {
   public:
-    Type
-    type() const override final;
-
     dtl::unique_variant_ptr_t<const Expression> predicate;
 };
 
@@ -431,9 +275,6 @@ class WhereClause final : public Node {
 
 class GroupByClause final : public Node {
   public:
-    Type
-    type() const override final;
-
     bool consecutive;
     std::vector<dtl::unique_variant_ptr_t<const Expression>> pattern;
 };
@@ -442,9 +283,6 @@ class GroupByClause final : public Node {
 
 class SelectExpression final : public Node {
   public:
-    Type
-    type() const override final;
-
     std::unique_ptr<const DistinctClause> distinct; /* nullable */
     std::vector<dtl::unique_variant_ptr_t<const ColumnBinding>> columns;
     std::unique_ptr<const FromClause> source;
@@ -455,17 +293,11 @@ class SelectExpression final : public Node {
 
 class ImportExpression final : public Node {
   public:
-    Type
-    type() const override final;
-
     std::unique_ptr<const String> location;
 };
 
 class TableReferenceExpression final : public Node {
   public:
-    Type
-    type() const override final;
-
     std::string name;
 };
 
@@ -473,51 +305,33 @@ class TableReferenceExpression final : public Node {
 
 class AssignmentStatement final : public Node {
   public:
-    Type
-    type() const override final;
-
     std::unique_ptr<const TableName> target;
     dtl::unique_variant_ptr_t<const TableExpression> expression;
 };
 
 class UpdateStatement final : public Node {
   public:
-    Type
-    type() const override final;
-
     /* TODO */
 };
 
 class DeleteStatement final : public Node {
   public:
-    Type
-    type() const override final;
-
     /* TODO */
 };
 
 class InsertStatement final : public Node {
   public:
-    Type
-    type() const override final;
-
     /* TODO */
 };
 
 class ExportStatement final : public Node {
   public:
-    Type
-    type() const override final;
-
     std::unique_ptr<const String> location;
     dtl::unique_variant_ptr_t<const TableExpression> expression;
 };
 
 class BeginStatement final : public Node {
   public:
-    Type
-    type() const override final;
-
     std::string text;
 };
 
@@ -534,9 +348,6 @@ typedef std::variant<
 
 class Script final : public Node {
   public:
-    Type
-    type() const override final;
-
     std::vector<dtl::unique_variant_ptr_t<const Statement>> statements;
 };
 
