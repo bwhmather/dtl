ec0963ea3a3ff32c9c3f1dba0ab3cde16b4f73e7
Ben Mather <bwhmather@bwhmather.com>
2024-07-01 18:12:03 +0100

    Factor out dtype enum and helpers

--- src/dtl-dtype.h
+++ src/dtl-dtype.h
@@ -1,5 +1,9 @@
 #pragma once
 
+#include <stdbool.h>
+#include <stddef.h>
+#include <stdint.h>
+
 enum dtl_dtype {
     DTL_DTYPE_BOOL,
     DTL_DTYPE_INT,
@@ -8,3 +12,30 @@ enum dtl_dtype {
     DTL_DTYPE_BYTES,
     DTL_DTYPE_INDEX,
 };
+
+bool
+dtl_get_bool(void const *data, size_t index);
+void
+dtl_set_bool(void *data, size_t index, bool value);
+
+uint64_t
+dtl_get_int(void const *data, size_t index);
+void
+dtl_set_int(void *data, size_t index, uint64_t value);
+
+double
+dtl_get_double(void const *data, size_t index);
+void
+dtl_set_double(void *data, size_t index, double value);
+
+char const *
+dtl_get_text(void const *data, size_t index);
+void
+dtl_set_text(void *data, size_t index, char const *value);
+
+// TODO decide byte representation.
+
+size_t
+dtl_get_index(void const *data, size_t index);
+void
+dtl_set_index(void *data, size_t index, size_t value);
