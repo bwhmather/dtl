a775e1ed58c7e4cb92cb847c29440f071d170ac6
Ben Mather <bwhmather@bwhmather.com>
2024-10-23 18:03:40 +0100

    Move stuff back to tracer

--- src/dtl-io.c
+++ src/dtl-io.c
@@ -6,6 +6,7 @@
 
 #include "dtl-dtype.h"
 #include "dtl-error.h"
+#include "dtl-location.h"
 #include "dtl-schema.h"
 #include "dtl-value.h"
 
@@ -83,20 +84,123 @@ dtl_io_exporter_export_table(
 
 /* === Tracers ================================================================================== */
 
-void
-dtl_io_tracer_write_manifest(struct dtl_io_tracer *tracer, struct dtl_io_trace_manifest *manifest) {
+enum dtl_status
+dtl_io_tracer_record_source(
+    struct dtl_io_tracer *tracer,
+    char const *source,
+    char const *filename,
+    struct dtl_error **error
+) {
     assert(tracer != NULL);
-    assert(tracer->write_manifest != NULL);
-    assert(manifest != NULL);
+    assert(source != NULL);
+    assert(filename != NULL);
+    assert(error != NULL);
 
-    tracer->write_manifest(tracer, manifest);
+    if (tracer->record_source != NULL) {
+        return tracer->record_source(tracer, source, filename, error);
+    }
+    return DTL_STATUS_OK;
 }
 
-void
-dtl_io_tracer_write_array(struct dtl_io_tracer *tracer, uint64_t id, enum dtl_dtype dtype, size_t length, void *data) {
+enum dtl_status
+dtl_io_tracer_record_input(
+    struct dtl_io_tracer *tracer,
+    char const *name,
+    struct dtl_schema *schema,
+    uint64_t *array_ids,
+    struct dtl_error **error
+) {
+    assert(tracer != NULL);
+    assert(name != NULL);
+    assert(schema != NULL);
+    assert(array_ids != NULL);
+    assert(error != NULL);
+
+    if (tracer->record_input != NULL) {
+        return tracer->record_input(tracer, name, schema, array_ids, error);
+    }
+    return DTL_STATUS_OK;
+}
+
+enum dtl_status
+dtl_io_tracer_record_output(
+    struct dtl_io_tracer *tracer,
+    char const *name,
+    struct dtl_schema *schema,
+    uint64_t *array_ids,
+    struct dtl_error **error
+) {
+    assert(tracer != NULL);
+    assert(name != NULL);
+    assert(schema != NULL);
+    assert(array_ids != NULL);
+    assert(error != NULL);
+
+    if (tracer->record_output != NULL) {
+        return tracer->record_output(tracer, name, schema, array_ids, error);
+    }
+    return DTL_STATUS_OK;
+}
+
+enum dtl_status
+dtl_io_tracer_record_trace(
+    struct dtl_io_tracer *tracer,
+    struct dtl_location start,
+    struct dtl_location end,
+    struct dtl_schema *schema,
+    uint64_t *array_ids,
+    struct dtl_error **error
+) {
+    assert(tracer != NULL);
+    assert(!dtl_location_is_null(start));
+    assert(!dtl_location_is_null(end));
+    assert(start.filename == end.filename);
+    assert(start.offset < end.offset);
+    assert(schema != NULL);
+    assert(array_ids != NULL);
+
+    if (tracer->record_trace != NULL) {
+        return tracer->record_trace(tracer, start, end, schema, array_ids, error);
+    }
+    return DTL_STATUS_OK;
+}
+
+enum dtl_status
+dtl_io_tracer_record_mapping(
+    struct dtl_io_tracer *tracer,
+    uint64_t src_array_id,
+    uint64_t tgt_array_id,
+    uint64_t src_index_array_id,
+    uint64_t tgt_index_array_id,
+    struct dtl_error **error
+) {
+    assert(tracer != NULL);
+    assert(src_array_id != 0);
+    assert(tgt_array_id != 0);
+
+    if (tracer->record_mapping != NULL) {
+        return tracer->record_mapping(tracer, src_array_id, tgt_array_id, src_index_array_id, tgt_index_array_id, error);
+    }
+    return DTL_STATUS_OK;
+}
+
+enum dtl_status
+dtl_io_tracer_record_value(
+    struct dtl_io_tracer *tracer,
+    uint64_t id,
+    enum dtl_dtype dtype,
+    size_t size,
+    struct dtl_value *value,
+    struct dtl_error **error
+) {
     assert(tracer != NULL);
     assert(id != 0);
-    assert(data != NULL || length == 0);
+    assert(dtl_dtype_is_array_type(dtype) || size == 0);
+    assert(value != NULL);
+    assert(error != NULL);
 
-    tracer->write_array(tracer, id, dtype, length, data);
+    if (tracer->record_value != NULL) {
+        return tracer->record_value(tracer, id, dtype, size, value, error);
+    }
+    return DTL_STATUS_OK;
 }
--- src/dtl-io.h
+++ src/dtl-io.h
@@ -50,48 +50,29 @@ dtl_io_exporter_export_table(struct dtl_io_exporter *, char const *, struct dtl_
 
 /* === Tracers ================================================================================== */
 
-struct dtl_io_trace_column {
-    char const *name;
-    uint64_t array;
-};
-
-struct dtl_io_trace_snapshot {
-    struct dtl_location start;
-    struct dtl_location end;
-
-    struct dtl_io_trace_column *columns;
-    size_t num_columns;
-};
-
-struct dtl_io_trace_mapping {
-    // The identifiers of the two arrays that this mapping connects.
-    uint64_t src_array;
-    uint64_t tgt_array;
-
-    // TODO more efficient representations for common cases.
-    // Pair of arrays forming a table that maps indexes in the source array into
-    // indexes in the target array.
-    uint64_t src_index_array;
-    uint64_t tgt_index_array;
-};
-
-struct dtl_io_trace_manifest {
-    char const *source;
-
-    struct dtl_io_trace_snapshot *snapshots;
-    size_t num_snapshots;
-
-    struct dtl_io_trace_mapping *mappings;
-    size_t num_mappings;
-};
-
 struct dtl_io_tracer {
-    void (*write_manifest)(struct dtl_io_tracer *, struct dtl_io_trace_manifest *);
-    void (*write_array)(struct dtl_io_tracer *, uint64_t, enum dtl_dtype, size_t, void *);
+    enum dtl_status (*record_source)(struct dtl_io_tracer *, char const *source, char const *filename, struct dtl_error **);
+    enum dtl_status (*record_input)(struct dtl_io_tracer *, char const *, struct dtl_schema *, uint64_t *, struct dtl_error **);
+    enum dtl_status (*record_output)(struct dtl_io_tracer *, char const *, struct dtl_schema *, uint64_t *, struct dtl_error **);
+    enum dtl_status (*record_trace)(struct dtl_io_tracer *, struct dtl_location start, struct dtl_location end, struct dtl_schema *, uint64_t *, struct dtl_error **);
+    enum dtl_status (*record_mapping)(struct dtl_io_tracer *, uint64_t src_array, uint64_t tgt_array, uint64_t src_index_array, uint64_t tgt_index_array, struct dtl_error **);
+    enum dtl_status (*record_value)(struct dtl_io_tracer *, uint64_t id, enum dtl_dtype, size_t, struct dtl_value *, struct dtl_error **);
 };
 
-void
-dtl_io_tracer_write_manifest(struct dtl_io_tracer *, struct dtl_io_trace_manifest *);
+enum dtl_status
+dtl_io_tracer_record_source(struct dtl_io_tracer *, char const *source, char const *filename, struct dtl_error **);
 
-void
-dtl_io_tracer_write_array(struct dtl_io_tracer *, uint64_t, enum dtl_dtype, size_t, void *);
+enum dtl_status
+dtl_io_tracer_record_input(struct dtl_io_tracer *, char const *, struct dtl_schema *, uint64_t *, struct dtl_error **);
+
+enum dtl_status
+dtl_io_tracer_record_output(struct dtl_io_tracer *, char const *, struct dtl_schema *, uint64_t *, struct dtl_error **);
+
+enum dtl_status
+dtl_io_tracer_record_trace(struct dtl_io_tracer *, struct dtl_location start, struct dtl_location end, struct dtl_schema *, uint64_t *, struct dtl_error **);
+
+enum dtl_status
+dtl_io_tracer_record_mapping(struct dtl_io_tracer *tracer, uint64_t src_array, uint64_t tgt_array, uint64_t src_index_array, uint64_t tgt_index_array, struct dtl_error **);
+
+enum dtl_status
+dtl_io_tracer_record_value(struct dtl_io_tracer *, uint64_t id, enum dtl_dtype, size_t, struct dtl_value *, struct dtl_error **);
